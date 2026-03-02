934f3c546c7568f26211e8bd3a833e1f57d9da50
Arthur Evangelista Oliveira <arturevangelistaoliveira@gmail.com>
2026-03-01 22:20:59 -0300

    Iniciado o projeto

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+// Importação de páginas de funcionalidade
+import { Home } from '@/app/Home';
+
+export default function App() {
+  return (
+    <View>
+     <Home/>
+    </View>
+  );
+}
